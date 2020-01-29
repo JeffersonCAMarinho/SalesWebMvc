a0a80c994e5850a05f2103eff9ae423c5a5ee2b4
jeffinhocorrea <jeffersonmarinho97@gmail.com>
2020-01-29 14:52:58 -0300

    foreign key not null(referential integrity)

--- Models/Seller.cs
+++ Models/Seller.cs
@@ -17,6 +17,7 @@ namespace SalesWebMvc.Models
 
         public Department Department { get; set; }
 
+        public int DepartmentID { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller()
